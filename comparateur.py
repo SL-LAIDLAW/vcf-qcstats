ce6497274ecc0133909021267dbe414e7de91675
Sean Laidlaw <sean.laidlaw@etu.umontpellier.fr>
2017-06-21 11:42:57 +0200

    added f-score statistics

--- comparateur.py
+++ comparateur.py
@@ -9,6 +9,8 @@ def stat_program(filename):
 	control = open(sys.argv[2], 'r')
 	FP_log_name = str(noext_filename) + "_false_positives.txt"
 	FP_log = open(FP_log_name,'w')
+	FN_log_name = str(noext_filename) + "_false_negatives.txt"
+	FN_log = open(FN_log_name,'w')
 	FP_log_regex = open(str(noext_filename) + "_false_positives" + "_regex.txt",'w')
 	matched_log_name = str(noext_filename) + "_matched_results.txt"
 	matched_log = open(matched_log_name,'w')
@@ -36,14 +38,16 @@ def stat_program(filename):
 			split_line = line.split("\t")
 			sample_chr = split_line[0]
 			sample_pos = split_line[1]
+			print(str(sample_pos))
 			sample_alt = split_line[3]
 			sample_id = str(sample_chr) + "_" + str(sample_pos) + "_" + str(sample_alt)
 			sample_id = sample_id.replace(" ", "")
+			sample_id = sample_id.upper()
 			FP_log_regex.write("(" + str(sample_pos) + ")|")
 			if sample_id not in sample_id_list:
 				sample_id_list.append(sample_id)
 
-
+	print("\n\n\n")
 
 	# Find true positives and false negatives by comparing Control > sample
 	for line in control:
@@ -52,10 +56,12 @@ def stat_program(filename):
 			split_line = line.split("\t")
 			control_chr = split_line[0]
 			control_pos = split_line[1]
+			print(str(control_pos))
 			control_alt = split_line[3]
 			control_origin = split_line[7]
 			control_id = str(control_chr) + "_" + str(control_pos) + "_" + str(control_alt)
 			control_id = control_id.replace(" ", "")
+			control_id = control_id.upper()
 			if control_id not in control_id_list:
 				control_id_list.append(control_id)
 				origin_dict.update({control_id:control_origin})
@@ -63,6 +69,7 @@ def stat_program(filename):
 				true_positive += 1
 			else:
 				false_negative += 1
+				FN_log.write(line)
 
 
 
@@ -91,10 +98,15 @@ def stat_program(filename):
 	results_log.write("\n False Positives : " + str(false_positive))
 	print(" False Positives : " + str(false_positive))
 	results_log.write("\n False Positives : " + str(false_positive))
-	print(" Sensitivity : " + str(100 * true_positive / (true_positive + false_negative)) + " %")
-	results_log.write("\n Sensitivity : " + str(100 * true_positive / (true_positive + false_negative)) + " %")
-	print(" Precision : " + str(100 * true_positive / (true_positive + false_positive)) + " %\n")
-	results_log.write("\n Precision : " + str(100 * true_positive / (true_positive + false_positive)) + " %\n")
+	result_sensitivity = 100 * true_positive / (true_positive + false_negative)
+	print(" Sensitivity : " + str(result_sensitivity) + " %")
+	results_log.write("\n Sensitivity : " + str(result_sensitivity) + " %")
+	result_precision = 100 * true_positive / (true_positive + false_positive)
+	print(" Precision : " + str(result_precision) + " %")
+	results_log.write("\n Precision : " + str(result_precision) + " %")
+	print(" F-Score : " + str(2*((result_precision * result_sensitivity)/(result_precision + result_sensitivity)) ))
+	results_log.write("\n F-Score : " + str(2*((result_precision * result_sensitivity)/(result_precision + result_sensitivity)) ))
+
 
 	FP_log.close()
 	FP_log_regex.close()

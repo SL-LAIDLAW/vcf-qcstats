bcc4b59410274e9f471a0ff62be12c31e92ff87c
Sean Laidlaw <sean.laidlaw@etu.umontpellier.fr>
2017-06-29 17:03:15 +0200

    Give stats on germline/somatic ratio

--- comparateur.py
+++ comparateur.py
@@ -15,7 +15,6 @@ def stat_program(filename):
 	quality_table = open(quality_table_name,'w')
 
 	if snakefile == False:
-			quality_table = open("variants_amp80_quality_table.txt",'w')
 			FP_log_name = str(noext_filename) + "_false_positives.txt"
 			FP_log = open(FP_log_name,'w')
 			FN_log_name = str(noext_filename) + "_false_negatives.txt"
@@ -29,7 +28,6 @@ def stat_program(filename):
 			quality_FP_log = open(quality_FP_log_name,'w')
 
 
-
 	# variables
 	origin_dict = {}
 	sample_id_list = []
@@ -41,16 +39,24 @@ def stat_program(filename):
 	sample_list_count = 0
 	true_positive_verify = 0
 
+	TP_cloneG = 0
+	TP_cloneB = 0
+	TP_cloneA = 0
+	FN_cloneG = 0
+	FN_cloneB = 0
+	FN_cloneA = 0
+
 	cmd = str("sort -u " + str(filename) + " > " + str(noext_filename) + "_sorted.vcf")
 	os.system(cmd)
-
 	sample.close()
 	sample = open(str(noext_filename) + "_sorted.vcf", 'r')
+
+	quality_table.write("quality" + "\t" + "category" + "\n")
 
 	# Create sample list to later be deduplicated
 	sampleExt = str(args['sample'])
 	sampleExt = sampleExt[-3:]
-	print(sampleExt)
+
 	# if vcf then use standardised vcf columns
 	if sampleExt == "vcf":
 		for line in sample:
@@ -58,7 +64,6 @@ def stat_program(filename):
 				split_line = line.split("\t")
 				sample_chr = split_line[0]
 				sample_pos = split_line[1]
-				# print(str(sample_pos))
 				sample_alt = split_line[3]
 				sample_qual = split_line[5]
 				sample_id = str(sample_chr) + "_" + str(sample_pos) + "_" + str(sample_alt)
@@ -72,8 +77,6 @@ def stat_program(filename):
 					quality_dict.update(quality_dict_entry)
 
 
-	print("\n\n\n")
-
 	# Find true positives and false negatives by comparing Control > sample
 	for line in control:
 		if not line.startswith("#", 0, 1):
@@ -81,9 +84,9 @@ def stat_program(filename):
 			split_line = line.split("\t")
 			control_chr = split_line[0]
 			control_pos = split_line[1]
-			print(str(control_pos))
 			control_alt = split_line[3]
 			control_origin = split_line[7]
+			control_origin = str(control_origin).upper()
 			control_id = str(control_chr) + "_" + str(control_pos) + "_" + str(control_alt)
 			control_id = control_id.replace(" ", "")
 			control_id = control_id.upper()
@@ -92,8 +95,21 @@ def stat_program(filename):
 				origin_dict.update({control_id:control_origin})
 			if control_id in sample_id_list:
 				true_positive += 1
+				if "CLONE=B" in control_origin:
+					TP_cloneB += 1
+				elif "CLONE=A" in control_origin:
+					TP_cloneA += 1
+				elif "CLONE=GERMLINE|A|B" in control_origin:
+					TP_cloneG += 1
+
 			else:
 				false_negative += 1
+				if "CLONE=B" in control_origin:
+					FN_cloneB += 1
+				elif "CLONE=A" in control_origin:
+					FN_cloneA += 1
+				elif "CLONE=G" in control_origin:
+					FN_cloneG += 1
 				if snakefile == False:
 					FN_log.write(line)
 
@@ -120,16 +136,19 @@ def stat_program(filename):
 		# Print results
 		print("\n ### Results for sample : " + str(filename))
 		results_log.write("\n ### Results for sample : " + str(filename))
-		print(" True Pos : " + str(true_positive) + " = " + str(true_positive_verify))
-		results_log.write("\n True Pos : " + str(true_positive) + " = " + str(true_positive_verify))
-		print(" False Neg : " + str(false_negative))
-		results_log.write("\n False Neg : " + str(false_negative))
 		print(" Total mutations in Control : " + str(false_negative + true_positive) + " = " + str(control_id_count))
 		results_log.write("\n Total mutations in Control : " + str(false_negative + true_positive) + " = " + str(control_id_count))
 		print(" Total mutations in Sample : " + str(sample_list_count))
-		results_log.write("\n False Positives : " + str(false_positive))
+
+		print("\n #### Global Statistics")
+		results_log.write("\n\n #### Global Statistics")
+		print(" True Positives : " + str(true_positive) + " = " + str(true_positive_verify))
+		results_log.write("\n True Positives : " + str(true_positive) + " = " + str(true_positive_verify))
 		print(" False Positives : " + str(false_positive))
 		results_log.write("\n False Positives : " + str(false_positive))
+		print(" False Negatives : " + str(false_negative))
+		results_log.write("\n False Negatives : " + str(false_negative))
+
 		result_sensitivity = 100 * true_positive / (true_positive + false_negative)
 		print(" Sensitivity : " + str(result_sensitivity) + " %")
 		results_log.write("\n Sensitivity : " + str(result_sensitivity) + " %")
@@ -139,15 +158,25 @@ def stat_program(filename):
 		print(" F-Score : " + str(2*((result_precision * result_sensitivity)/(result_precision + result_sensitivity)) ))
 		results_log.write("\n F-Score : " + str(2*((result_precision * result_sensitivity)/(result_precision + result_sensitivity)) ) + "\n")
 
+		print("\n #### Origin Breakdown")
+		results_log.write("\n\n #### Origin Breakdown")
+		print(" Germline Matches : " + str(TP_cloneG))
+		print(" Somatic Matches : " + str(TP_cloneA + TP_cloneB))
+		results_log.write("\n Proportion of Germline matches : " + str(100* TP_cloneG / (TP_cloneB + TP_cloneA + TP_cloneG)) + " %")
+		print(" Proportion of Germline matches : " + str(100* TP_cloneG / (TP_cloneB + TP_cloneA + TP_cloneG)) + " %")
+		results_log.write("\n Germline Sensitivity : " + str(100 * TP_cloneG / (TP_cloneG + FN_cloneG)))
+		print(" Germline Sensitivity : " + str(100 * TP_cloneG / (TP_cloneG + FN_cloneG)) + " %")
+		results_log.write("\n Somatic Sensitivity : " + str(100 * (TP_cloneB + TP_cloneA) / ((TP_cloneB + TP_cloneA) + (FN_cloneB + FN_cloneA))))
+		print(" Somatic Sensitivity : " + str(100 * (TP_cloneB + TP_cloneA) / ((TP_cloneB + TP_cloneA) + (FN_cloneB + FN_cloneA))) + " %")
+
 
 		FP_log.close()
 		FP_log_regex.close()
 		matched_log.close()
 		quality_TP_log.close()
 
-		testsubject = str(args['control'])
 		FP_log_regex = open((str(noext_filename) + "_false_positives" + "_regex.txt"),'r').read()
-		FP_log_regex = "sed \"/" + FP_log_regex + "/p\" " + str(testsubject) + " > " + str(testsubject) + "_matches.txt"
+		FP_log_regex = "sed \"/" + FP_log_regex + "/p\" " + str(str(args['control'])) + " > " + str(str(args['control'])) + "_matches.txt"
 		# print(FP_log_regex)
 # End of function
 
